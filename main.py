3b477acc97971c79421801cb0843559b4a055912
Ionescu Roxana <roxanaionescu08@yahoo.ro>
2026-05-25 14:38:56 +0300

    main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ print(df.info())
 print(df["type"].value_counts())
 # top 10 tari
 print(df["country"].value_counts().head(10))
+
 print(df["release_year"].value_counts().head(10))
 print(df["rating"].value_counts())
 
@@ -25,3 +26,30 @@ plt.xlabel("Type")
 plt.ylabel("Count")
 
 plt.show()
+
+df["country"].value_counts().head(10).plot(kind="bar")
+
+plt.title("Top 10 Countries")
+
+plt.show()
+recent_movies = df[df["release_year"] > 2020]
+
+print(recent_movies.head())
+
+df["release_year"].value_counts().head(10).plot(kind="bar")
+
+plt.title("Top Release Years")
+
+plt.show()
+
+
+movies = df[df["type"] == "Movie"]
+
+print(movies.head())
+
+usa_content = df[df["country"] == "United States"]
+
+print(usa_content.head())
+recent = df[df["release_year"] > 2018]
+
+print(recent.head())
